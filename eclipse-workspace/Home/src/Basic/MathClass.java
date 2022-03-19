d876819697552c77b1622d59de6625687b0b00aa
Ayaan Arvin <Ayaan Arvin@Ayaan-Arvin>
2022-03-18 22:04:09 -0400

    first commit

---
+++ eclipse-workspace/Home/src/Basic/MathClass.java
@@ -0,0 +1,23 @@
+package Basic;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=20;
+		int y=-10;
+		
+		int max=Math.max (x,y);
+		System.out.println("maximum"+max);
+		
+		int min=Math.min (x,y);
+		System.out.println("minimum"+min);
+	
+		int absolute=Math.abs (y);
+		System.out.println("absolute of y="+absolute);	
+		
+		
+
+	}
+
+}
---
+++ eclipse-workspace/Home/src/Basic/wrapperdemo.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class wrapperdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive-> object
+		int x=40;
+		Integer y=x;
+		System.out.println("y="+x);//autoboxing, converting primitive to object
+
+		double m=10.2;
+		Double n=m;
+		System.out.println("n="+m);//unboxing, converting object to primitive
+	
+		
+		
+		
+		
+	}
+
+}
